546056d78b74de81b934098b0d5f798a1dba6c93
Daphne Rutnam <daphne.rutnam@gmail.com>
2026-02-27 14:12:23 +0100

    Ensure dtype string to aid cleaning

--- 1_Cleaning/create_variable_from_survey.py
+++ 1_Cleaning/create_variable_from_survey.py
@@ -1,4 +1,5 @@
 # Function to create variable(s) from cell(s) in individual survey files
+# All variables created as string type to aid cleaning
 def create_var(ws, labs, mask, var_name,
                cell,
                multiple_cells=False,
@@ -10,30 +11,30 @@ def create_var(ws, labs, mask, var_name,
     if multiple_cells:
         cells = ws[cell]  # cell range
         values = [c.value for row in cells for c in row]
-        values = [v for v in values]
+        values = [str(v) if v is not None else "" for v in values]
 
         if comment_cell:
             comment_cells = ws[comment_cell] # comment range
             comment_values = [c.value for row in comment_cells for c in row]
-            comment_values = [v for v in comment_values]
+            comment_values = [str(v) if v is not None else "" for v in comment_values]
 
         if fc_cell:
             fc_cells = ws[fc_cell] # free text range
             fc_values = [c.value for row in fc_cells for c in row]
-            fc_values = [v for v in fc_values]
+            fc_values = [str(v) if v is not None else "" for v in fc_values]
 
         # Create separate variables per cell (cell, comment, free text)
         if no_variables: 
             for i, v in enumerate(values, start=1):
-                labs.loc[mask, f"{var_name}_{i}"] = v
+                labs.loc[mask, f"{var_name}_{i}"] = str(v) if v is not None else ""
 
             if comment_cell:
                 for i, v in enumerate(comment_values, start=1):
-                    labs.loc[mask, f"{var_name}_{i}_co"] = v
+                    labs.loc[mask, f"{var_name}_{i}_co"] = str(v) if v is not None else ""
             
             if fc_cell:
                 for i, v in enumerate(fc_values, start=1):
-                    labs.loc[mask, f"{var_name}_{i}_fc"] = v
+                    labs.loc[mask, f"{var_name}_{i}_fc"] = str(v) if v is not None else ""
 
         # Create single variable with all cells joined by ";"
         else:
@@ -48,14 +49,14 @@ def create_var(ws, labs, mask, var_name,
     # Single cell
     else:
         v = ws[cell].value
-        labs.loc[mask, var_name] = v
+        labs.loc[mask, var_name] = str(v) if v is not None else ""
 
         # Comment
         if comment_cell:
             v = ws[comment_cell].value
-            labs.loc[mask, f"{var_name}_co"] = v
+            labs.loc[mask, f"{var_name}_co"] = str(v) if v is not None else ""
 
         # Free text response
         if fc_cell:
             v = ws[fc_cell].value
-            labs.loc[mask, f"{var_name}_fc"] = v
+            labs.loc[mask, f"{var_name}_fc"] = str(v) if v is not None else ""
